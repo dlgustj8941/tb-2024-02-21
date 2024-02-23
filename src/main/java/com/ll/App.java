96e66ba08aa25f2109b22f2d54b81a6987b7fb0d
dlgustj8941 <dlgustj8941@gmail.com>
2024-02-23 21:33:37 +0900

    등록이라고 하면 명언과 작가가 나오게 한다.

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -14,6 +14,15 @@ class App {
             if (cmd.equals("종료")) {
                 break;
             }
+            else if (cmd.equals("등록")) {
+                System.out.printf("등록할 명언을 작성하세요 : ");
+                String 명언 = scanner.nextLine();
+                System.out.printf("등록한 명언의 작가를 작성하세요 : ");
+                String 작가 = scanner.nextLine();
+                System.out.println("명언 : " + 명언);
+                System.out.println("작가 : " + 작가);
+                break;
+            }
 
             System.out.printf("입력하신 명령 : %s\n", cmd);
         }
